b0075790c6509c4824a54b9baf36ecaba03a928f
Tanya <tanya011btcse20@gmail.com>
2024-03-16 16:34:59 +0530

    login and register

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import '../styles/navbar.scss'
+
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, useNavigate } from "react-router-dom"
+
+
+
+const Navbar = () => {
+
+    return (
+        <div className='navContainer'>
+            <Link to="/home"> 
+                <p className='navLogo'>JourneyDash</p>
+            </Link>
+
+            <input type="checkbox" id='menu-bar' />
+            <label htmlFor="menu-bar"><FontAwesomeIcon icon={faBars} className="icon" /></label>
+            <nav className='navbar'>
+                <ul>
+                    <Link to="/dashboard">
+                        <li><p>Dashboard</p></li>
+                    </Link>
+                    <Link to="/login">
+                        <li><p>Login</p></li>
+                    </Link>
+                    <Link to="/register">
+                        <li><p>Register</p></li>
+                    </Link>
+                </ul>
+            </nav>
+        </div >
+    )
+}
+
+export default Navbar
---
+++ client/src/pages/Destination.jsx
@@ -0,0 +1,58 @@
+import React, {useState, useEffect} from 'react'
+import Map, {Marker} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+const Destination = () => {
+  const [places, setPlaces] = useState([]);
+  const [value, setValue] = useState("")
+  useEffect(() =>{
+    getPlaces();
+  },[value])
+  console.log(places)
+
+  const getPlaces = async() => {
+    const promise = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${value}.json?access_token=pk.eyJ1IjoidGFuZ2xlZHZpbmV6eiIsImEiOiJjbHRzdjRtYzAwcjl2Mm5vYW1hNDhza3kxIn0.GB7H7hpV4fw2CMRGQppeKw`)
+    const data = await promise.json();
+    setPlaces(data.features);
+  }
+
+  const handleClick = () => {
+
+  }
+
+  return (
+    <div>
+      <div className="display-map">
+        <input placeholder='Enter endpoint' type="text" name='place' id='place' value={value} onChange={(e) => setValue(e.target.value)}/>
+      </div>
+
+      <div className="">
+        {
+          places?.map((items, index) => {
+            return (
+              <div key={index} onClick={() =>handleClick(items)}>
+                <p>{items.place_name}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+
+      <div className="map">
+      <Map
+        initialViewState={{
+          latitude: 37.8,
+          longitude: -122.4,
+          zoom: 14
+        }}
+        style={{width: 800, height: 600}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken="pk.eyJ1IjoidGFuZ2xlZHZpbmV6eiIsImEiOiJjbHRzdjRtYzAwcjl2Mm5vYW1hNDhza3kxIn0.GB7H7hpV4fw2CMRGQppeKw"
+      >
+        <Marker className="marker" longitude={77.2090057} latitude={28.6138954} color="red" />
+      </Map>
+      </div>
+    </div>
+  )
+}
+
+export default Destination
